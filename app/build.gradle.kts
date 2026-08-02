d67b0ff491152aafee7a1262d3e66123c301d5d8
Frostre_ <n9043395@gmail.com>
2026-08-02 20:55:34 +0200

    Downgrade Kotlin compiler extension version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         compose = true
     }
     composeOptions {
-       kotlinCompilerExtensionVersion = "1.9.20"
+       kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     compileOptions {
