2a27595bad14501d43cd3f4117fd98cf1a5411d0
Frostre_ <n9043395@gmail.com>
2026-08-02 20:54:26 +0200

    Update Kotlin compiler extension version to 1.9.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         compose = true
     }
     composeOptions {
-       kotlinCompilerExtensionVersion = "1.5.4"
+       kotlinCompilerExtensionVersion = "1.9.20"
     }
 
     compileOptions {
