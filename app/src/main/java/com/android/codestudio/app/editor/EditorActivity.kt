ec83d34ee9efc21d2f253911633785b6e4b7dcf8
Frostre_ <n9043395@gmail.com>
2026-08-02 20:49:09 +0200

    Migrate EditorActivity to Jetpack Compose
    Refactor EditorActivity to use Jetpack Compose and AndroidView.

--- app/src/main/java/com/android/codestudio/app/editor/EditorActivity.kt
+++ app/src/main/java/com/android/codestudio/app/editor/EditorActivity.kt
@@ -3,17 +3,33 @@ package com.android.codestudio.app
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 
-class EditorActivity : AppCompatActivity() {
+class EditorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_editor)
-
-        val webView = findViewById<WebView>(R.id.webView)
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("file:///android_asset/editor.html")
+        setContent {
+            EditorScreen()
+        }
     }
+}
+
+@Composable
+fun EditorScreen() {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl("file:///android_asset/editor.html")
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
 }
