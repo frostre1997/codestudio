9e598200473fa1941250bf1065055a5dde6981d4
Frostre_ <n9043395@gmail.com>
2026-08-02 18:38:22 +0200

    Change preference retrieval method in MainActivity

--- app/src/main/java/com/android/codestudio/app/MainActivity.kt
+++ app/src/main/java/com/android/codestudio/app/MainActivity.kt
@@ -3,23 +3,19 @@ package com.android.codestudio.app
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Check if the user has completed setup
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val prefs = getSharedPreferences("codestudio_prefs", MODE_PRIVATE)
         val isSetupComplete = prefs.getBoolean("setup_complete", false)
 
         if (isSetupComplete) {
-            // Go straight to the editor
             startActivity(Intent(this, EditorActivity::class.java))
         } else {
-            // Show the setup wizard
             startActivity(Intent(this, SetupActivity::class.java))
         }
-        finish() // close MainActivity so user can't press back to it
+        finish()
     }
 }
