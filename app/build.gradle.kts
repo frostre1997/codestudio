84b8d37d258ce93f003e8c4fcd012fd767522cd4
Frostre_ <n9043395@gmail.com>
2026-08-02 20:46:06 +0200

    Add Compose support in build.gradle.kts
    Enable Jetpack Compose and set Kotlin compiler extension version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,13 @@ android {
         }
     }
 
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+       kotlinCompilerExtensionVersion = "1.5.4"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
