0e2617aa09776e483a3cfe6404f2f3452e709c90
Frostre_ <n9043395@gmail.com>
2026-08-02 20:38:41 +0200

    Remove unnecessary newline in MainActivity.kt

--- app/src/main/java/com/android/codestudio/app/MainActivity.kt
+++ app/src/main/java/com/android/codestudio/app/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
         startActivity(Intent(this, DashboardActivity::class.java))
         finish()
     }
