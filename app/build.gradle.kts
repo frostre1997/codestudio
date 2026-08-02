bb54090ae7c2aa6d4c39e0ebbc5e0ba57762f623
Frostre_ <n9043395@gmail.com>
2026-08-02 20:46:50 +0200

    Add Compose dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.graphics:graphics-path:1.0.1")
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.8.2")
 }
